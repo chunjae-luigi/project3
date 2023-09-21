53a30f2dce1bd6a2876d37b10ec29d8c7084f531
soumunda8 <soumunda8@naver.com>
2023-09-21 14:19:16 +0900

    finish : index.jsp

--- src/main/java/kr/co/tspoon/service/NoticeService.java
+++ src/main/java/kr/co/tspoon/service/NoticeService.java
@@ -8,6 +8,7 @@ import java.util.List;
 public interface NoticeService {
 
     public List<Notice> noticeList() throws Exception;
+    public List<Notice> noticeListForMain() throws Exception;
     public Notice noticeGet(int no) throws Exception;
     public void noticeInsert(Notice notice) throws Exception;
     public void noticeUpdate(Notice notice) throws Exception;
